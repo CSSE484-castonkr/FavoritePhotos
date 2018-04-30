b079c20bc65d93a7cfcf47f78448d662bf04eac4
Kiana Caston <castonkr@rose-hulman.edu>
2018-04-30 11:58:06 -0400

    User interface setup

---
+++ FavoritePhotos/FavoritePhotoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FavoritePhotoViewController.swift
+//  FavoritePhotos
+//
+//  Created by Kiana Caston on 4/30/18.
+//  Copyright © 2018 Kiana Caston. All rights reserved.
+//
+
+import UIKit
+
+class FavoritePhotoViewController: UIViewController {
+
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    @IBAction func takePhoto(_ sender: Any) {
+        
+        
+    }
+    
+}
